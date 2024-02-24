2b825c643b76200d5883a70c52b71e597f51bcb1
PhucPQ_00 <phucpq07122000@gmail.com>
2024-02-24 12:31:38 +0700

    Create DBcontext

---
+++ EFCore_CodeFirst/CodeFirstDBContext.cs
@@ -0,0 +1,18 @@
+﻿using EFCore_CodeFirst.Enitity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_CodeFirst
+{
+    public class CodeFirstDBContext : DbContext
+    {
+        public DbSet<User>? Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=ep-crimson-breeze-a1kxgcsp.ap-southeast-1.aws.neon.tech;Database=MyWork1;Username=phucpq07122000;Password=pMG49dRqZQBf");
+    }
+}
